7d54efdeb6528b7539c3068e2351a33547030ebf
rarhs <rarhs@users.noreply.github.com>
2023-01-09 07:00:12 +0000

    Add Google Home Page

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GoogleHomePage() {
+  return (
+    <div>
+      <h1>Google</h1>
+      <input type="text" placeholder="Search"></input>
+      <button>Google Search</button>
+      <button>I'm Feeling Lucky</button>
+    </div>
+  )
+}
+
+export default GoogleHomePage
